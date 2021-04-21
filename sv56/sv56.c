7ece60dd5a573408130a43f0cde8f061c9cb761d
youngjamespark <youngjamespark@gmail.com>
2021-04-21 14:44:16 +0900

    wave header read function modified

--- sv56/sv56.c
+++ sv56/sv56.c
@@ -3,6 +3,11 @@
 #include "sv56.h"
 #include <string.h>
 
+#define WAV_HEADER_NOK          -1
+#define WAV_HEADER_NOT_PCM      -2
+#define WAV_HEADER_NOT_MONO     -3
+#define WAV_HEADER_NOT_16BIT    -4
+
 int wav_header_read(char* FileIn, wav_header* header)
 {
     FILE* Fi;
@@ -15,7 +20,7 @@ int wav_header_read(char* FileIn, wav_header* header)
     fread(header->riff_header, sizeof(char), 4, Fi);
     if (strncmp((unsigned char*)header->riff_header, "RIFF", 4) != 0) {
         fprintf(stderr, "not RIFF,%s \n", header->riff_header);
-        return -1;
+        return WAV_HEADER_NOK;
     }
 
     // wave size = filesize - 8
@@ -25,14 +30,14 @@ int wav_header_read(char* FileIn, wav_header* header)
     fread(header->wave_header, sizeof(char), 4, Fi);
     if (strncmp((char*)header->wave_header, "WAVE", 4) != 0) {
         fprintf(stderr, "not WAVE\n");
-        return -1;
+        return WAV_HEADER_NOK;
     }
 
     // "fmt "
     fread(header->fmt_header, sizeof(char), 4, Fi);
     if (strncmp(header->fmt_header, "fmt ", 4) != 0) {
         fprintf(stderr, "not fmt \n");
-        return -1;
+        return WAV_HEADER_NOK;
     }
 
     // chunk size = 16 or 18
@@ -42,27 +47,27 @@ int wav_header_read(char* FileIn, wav_header* header)
     fread(&header->audio_format, sizeof(short), 1, Fi);
     if (header->audio_format != 1) {
         fprintf(stderr, "not PCM\n");
-        return -1;
+        return WAV_HEADER_NOT_PCM;
     }
 
     // channel number = 1
     fread(&header->num_channels, sizeof(short), 1, Fi);
     if (header->num_channels != 1) {
         fprintf(stderr, "not MONO channel\n");
-        return -1;
+        return WAV_HEADER_NOT_MONO;
     }
 
     // sample rate = 16000
     fread(&header->sample_rate, sizeof(int), 1, Fi);
     if (header->sample_rate != 16000) {
-        fprintf(stderr, "%d, not 16000Hz\n", header->sample_rate);
+        fprintf(stderr, "sample rate = %d, not 16000Hz\n", header->sample_rate);
         return -1;
     }
 
     // byte rate = 16000 * 2
     fread(&header->byte_rate, sizeof(int), 1, Fi);
     if (header->byte_rate != 32000) {
-        fprintf(stderr, "not correct byte rate, %d\n", header->byte_rate);
+        fprintf(stderr, "byte rate = %d, not wideband\n", header->byte_rate);
         return -1;
     }
 
@@ -71,6 +76,10 @@ int wav_header_read(char* FileIn, wav_header* header)
 
     // bit depth = 16
     fread(&header->bit_depth, sizeof(short), 1, Fi);
+    {
+        fprintf(stderr, "not 16 bit data\n");
+        return WAV_HEADER_NOT_16BIT;
+    }
 
     // two bytes chunk
     if (header->fmt_chunk_size == 0x12)
@@ -80,7 +89,7 @@ int wav_header_read(char* FileIn, wav_header* header)
     fread(header->data_header, sizeof(char), 4, Fi);
     if (strncmp(header->data_header, "data", 4) != 0) {
         fprintf(stderr, "not data\n");
-        return -1;
+        return WAV_HEADER_NOK;
     }
 
     // data bytes
